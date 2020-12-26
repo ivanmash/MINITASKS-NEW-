b0dcce54ca6706c3236fa015c521aa9ba2a9ccb8
ivanmash <iv.mashkarin@yandex.ru>
2020-12-26 22:07:48 +0300

    SDD

---
+++ Мнизадача_4_Машкарин_Иван/Мнизадача_4_Машкарин_Иван/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Мнизадача_4_Машкарин_Иван
+{
+    public partial class Form1 : Form
+    {
+        List<Вершины> S;
+        int shapenumb;
+        bool Draw;
+        public Form1()
+        {
+            InitializeComponent();
+            квадратToolStripMenuItem.CheckOnClick = true;
+            треугольникToolStripMenuItem.CheckOnClick = true;
+            кругToolStripMenuItem.CheckOnClick = true;
+            квадратToolStripMenuItem.Checked = true;
+            S = new List<Вершины>();
+            Draw = false;
+            shapenumb = 0;
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (Draw)
+            {
+                bool Up = false;
+                bool Down = false;
+                double d = 0;
+                Graphics g = e.Graphics;
+                foreach (Вершины Obj in S)
+                {
+                    Obj.InsideRrov = false;
+                    Obj.DrawFigure(g);
+                }
+                Pen pen = new Pen(Color.Orange);
+                if (S.Count() > 2)
+                {
+                        for (int i = 0; i < S.Count() - 1; i++)
+                        { 
+                            for (int j = i + 1; j < S.Count(); j++)
+                        {
+                                for (int k = 0; k < S.Count(); k++)
+                                {
+                                    if (k != j && k != i)
+                                    {
+                                        d = (S[k].SetCoorY - S[j].SetCoorY) * (S[i].SetCoorX - S[j].SetCoorX) - ((S[i].SetCoorY - S[j].SetCoorY) * (S[k].SetCoorX - S[j].SetCoorX));
+                                    if (d >= 0)
+                                        {
+                                            Up = true;
+                                        }
+                                        else Down = true;
+                                    }
+                                }
+                                if(!(Up && Down))
+                                {
+                                    S[i].InsideRrov = true;
+                                    S[j].InsideRrov = true;
+                                    e.Graphics.DrawLine(pen, S[i].SetCoorX, S[i].SetCoorY, S[j].SetCoorX, S[j].SetCoorY);
+                                }
+                            Up = false;
+                            Down = false;
+                        }
+                        }
+                }
+            }
+        }
+
+            private void Form1_MouseDown(object sender, MouseEventArgs e)
+            {
+                bool specprov;
+                if (!S.Any() && e.Button == MouseButtons.Left)
+                {
+                    switch (shapenumb)
+                    {
+                        case (0): S.Add(new Квадрат(e.X, e.Y)); break;
+                        case (1): S.Add(new Треугольник(e.X, e.Y)); break;
+                        case (2): S.Add(new Круг(e.X, e.Y)); break;
+                    }
+                    Draw = true;
+                    Refresh();
+                }
+                else
+                {
+                    specprov = false;
+                    foreach (Вершины Obj in S)
+                    {
+                        if (Obj.CheckArea(e.X, e.Y) && Draw)
+                        {
+                            if (e.Button == MouseButtons.Left)
+                            {
+                                Obj.Sdvigx = e.X - Obj.SetCoorX;
+                                Obj.Sdvigy = e.Y - Obj.SetCoorY;
+                                Obj.BeingDragged = true;
+                                specprov = true;
+                            }
+                            if (e.Button == MouseButtons.Right)
+                            {
+                                S.Remove(Obj); Refresh(); specprov = true; break;
+                            }
+                        }
+                    }
+                if (e.Button == MouseButtons.Left && specprov == false)
+                {
+                    Draw = true;
+                    if (S.Count > 2 && Вершины.InFigure(e.X, e.Y, S))
+                    {
+                        foreach (Вершины Obj in S)
+                        {
+                            Obj.Sdvigx = e.X - Obj.SetCoorX;
+                            Obj.Sdvigy = e.Y - Obj.SetCoorY;
+                            Obj.BeingDragged = true;
+                        }
+                    }
+                    else
+                    {
+                        switch (shapenumb)
+                        {
+                            case (0): S.Add(new Квадрат(e.X, e.Y)); break;
+                            case (1): S.Add(new Треугольник(e.X, e.Y)); break;
+                            case (2): S.Add(new Круг(e.X, e.Y)); break;
+                        }
+                    }
+                }
+                Refresh();
+                }
+            }
+        private void Form1_Load(object sender, EventArgs e)
+            {
+                квадратToolStripMenuItem.Checked = true;
+                DoubleBuffered = true;
+            }
+            private void КвадратToolStripMenuItem_Click_1(object sender, EventArgs e)
+            {
+                shapenumb = 0;
+                треугольникToolStripMenuItem.Checked = false;
+                кругToolStripMenuItem.Checked = false;
+            }
+
+            private void ТреугольникToolStripMenuItem_Click(object sender, EventArgs e)
+            {
+                shapenumb = 1;
+                квадратToolStripMenuItem.Checked = false;
+                кругToolStripMenuItem.Checked = false;
+            }
+
+            private void КругToolStripMenuItem_Click(object sender, EventArgs e)
+            {
+                shapenumb = 2;
+                треугольникToolStripMenuItem.Checked = false;
+                квадратToolStripMenuItem.Checked = false;
+            }
+            private void Form1_MouseMove(object sender, MouseEventArgs e)
+            {
+                foreach (Вершины Obj in S)
+                {
+                    if (Obj.BeingDragged)
+                    {
+                        Obj.SetCoorX = e.X - Obj.Sdvigx;
+                        Obj.SetCoorY = e.Y - Obj.Sdvigy;
+                        Refresh();
+                    }
+                }
+            }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            foreach (Вершины Obj in S)
+            {
+                if (Obj.BeingDragged)
+                {
+                    Obj.BeingDragged = false;
+                }
+            }
+            if(S.Count > 2)
+            {
+                for (int i = 0; i < S.Count(); i++)
+                {
+                    if (!S[i].InsideRrov)
+                    {
+                        S.Remove(S[i]);
+                    }
+                }
+            }
+            Refresh();
+        }
+    }
+}
---
+++ Мнизадача_4_Машкарин_Иван/Мнизадача_4_Машкарин_Иван/Вершины.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Мнизадача_4_Машкарин_Иван
+{
+    abstract class Вершины
+    {
+        protected static int R;
+        protected int X;
+        protected int Y;
+        protected int sdvigx;
+        protected int sdvigy;
+        protected bool beingDragged;
+        public bool in_prov;
+        public abstract void DrawFigure(Graphics graf);
+        public abstract int Radius { set; }
+        public bool InsideRrov { get; set;}
+        public static bool InFigure(int x, int y, List<Вершины> polygons)
+        {
+                double d;
+                bool Up = false;
+                bool Down = false;
+                List<Вершины> newList = new List<Вершины>();
+                Вершины MOUSE = new Треугольник(x, y);
+                polygons.Add(MOUSE);
+                for (int i = 0; i < polygons.Count() - 1; i++)
+                {
+                    for (int j = i + 1; j < polygons.Count(); j++)
+                    {
+                        for (int k = 0; k < polygons.Count(); k++)
+                        {
+                            if (k != j && k != i)
+                            {
+                                d = (polygons[k].SetCoorY - polygons[j].SetCoorY) * (polygons[i].SetCoorX - polygons[j].SetCoorX) - ((polygons[i].SetCoorY - polygons[j].SetCoorY) * (polygons[k].SetCoorX - polygons[j].SetCoorX));
+                                if (d >= 0)
+                                {
+                                    Up = true;
+                                }
+                                else
+                                {
+                                    Down = true;
+                                }
+                            }
+                        }
+                        if (!(Up && Down))
+                        {
+                            newList.Add(polygons[i]);
+                            newList.Add(polygons[j]);
+                        }
+                        Up = false;
+                        Down = false;
+                    }
+                }
+                if (!newList.Contains(MOUSE))
+                {
+                    polygons.Remove(MOUSE);
+                    return true;
+                }
+            polygons.Remove(MOUSE);
+            return false;
+        }
+        public int Sdvigx
+        {
+            set { sdvigx = value; }
+            get { return sdvigx; }
+        }
+        public bool BeingDragged
+        {
+            set
+            {
+                beingDragged = value;
+            }
+            get
+            {
+                return beingDragged;
+            }
+        }
+        public int Sdvigy
+        {
+            set { sdvigy = value; }
+            get { return sdvigy; }
+        }
+        public Вершины(int x, int y)
+        {
+            X = x;
+            Y = y;
+            beingDragged = false;
+            sdvigx = 0;
+            sdvigy = 0;
+            in_prov = false;
+        }
+        static Вершины()
+        {
+            R = 40;
+        }
+        public int SetCoorX
+        {
+            set { X = value; }
+            get { return X; }
+        }
+        public int SetCoorY
+        {
+            set { Y = value; }
+            get { return Y; }
+        }
+        public abstract bool CheckArea(int x, int y);
+    }
+    class Квадрат : Вершины
+    {
+        public Квадрат(int x, int y) : base(x, y) { }
+
+        public override void DrawFigure(Graphics graf)
+        {
+            graf.FillRectangle(new SolidBrush(Color.Yellow), X - R / 2, Y - R / 2, R, R);
+        }
+        public override int Radius
+        {
+            set { R = value; }
+        }
+        public override bool CheckArea(int x, int y)
+        {
+            if (X + R / 2 > x && X - R / 2 < x && Y + R / 2 > y && Y - R / 2 < y)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+    class Треугольник : Вершины
+    {
+        public Треугольник(int x, int y) : base(x, y) { }
+        public override void DrawFigure(Graphics graf)
+        {
+            Point[] p3 = new Point[3];
+            p3[0] = new Point(X + R / 2 - R / 2, Y - R / 2);
+            p3[1] = new Point(X - R / 2, Y + R - R / 2);
+            p3[2] = new Point(X + R - R / 2, Y + R - R / 2);
+            graf.FillPolygon(new SolidBrush(Color.Yellow), p3);
+        }
+        public override int Radius
+        {
+            set { R = value; }
+        }
+        public override bool CheckArea(int x, int y)
+        {
+            int a = (X + R / 2 - R / 2 - x) * (Y + R - R / 2 - (Y - R / 2)) - (X - R / 2 - (X + R / 2 - R / 2)) * (Y - R / 2 - y);
+            int b = (X - R / 2 - x) * (Y + R - R / 2 - (Y + R - R / 2)) - (X + R - R / 2 - (X - R / 2)) * (Y + R - R / 2 - y);
+            int c = (X + R - R / 2 - x) * (Y - R / 2 - (Y + R - R / 2)) - (X + R / 2 - R / 2 - (X + R - R / 2)) * (Y + R - R / 2 - y);
+
+            if ((a >= 0 && b >= 0 && c >= 0) || (a <= 0 && b <= 0 && c <= 0))
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+    class Круг : Вершины
+    {
+        public Круг(int x, int y) : base(x, y) { }
+        public override void DrawFigure(Graphics graf)
+        {
+            graf.FillEllipse(new SolidBrush(Color.Yellow), X - R / 2, Y - R / 2, R, R);
+        }
+        public override int Radius
+        {
+            set { R = value; }
+        }
+        public override bool CheckArea(int x, int y)
+        {
+            if (Math.Pow(X - x, 2) + Math.Pow(Y - y, 2) <= Math.Pow(R / 2, 2))
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
